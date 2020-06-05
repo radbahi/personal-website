16dc25218f7a88633e44dcbec2a79e429c9e9285
Radouane Bahi <radbahi@gmail.com>
2020-06-05 12:02:08 -0400

    added projects

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,49 +1,56 @@
 import React from "react";
 import { Card, Button } from "react-bootstrap";
 
+const pokemonimg = require("../local_images/POKEMONIMG.png");
+const covidimg = require("../local_images/COVID19IMG.jpg");
+const hanximg = require("../local_images/HANXIMG.jpg");
 const cardStyle = {
-    display: "flex",
-    flexDirection: 'column',
-    width: "40rem",
-    alignItems: 'center',
-    textAlign: 'center',
-    justifyContent: 'center'
+  display: "flex",
+  flexDirection: "column",
+  width: "40rem",
+  alignItems: "center",
+  textAlign: "center",
+  justifyContent: "center",
 };
 
 const Projects = () => {
   return (
-    <div className='project-div'>
+    <div className="project-div">
       <Card style={cardStyle}>
-        <Card.Img variant="top" src="holder.js/100px180" />
+        <Card.Img variant="top" src={covidimg} />
         <Card.Body>
-          <Card.Title>Card Title</Card.Title>
+          <Card.Title>COVID-19 Tracker</Card.Title>
           <Card.Text>
-            Some quick example text to build on the card title and make up the
-            bulk of the card's content.
+            Aptly named. A world map with bubbles that you can hover over for
+            quick statistics in each country regarding the virus. Also a news
+            section which contains top international headlines.
           </Card.Text>
-          <Button variant="primary">Visit project</Button>
+          <Button target="_blank" href="https://covid19trackerflatiron.herokuapp.com/" variant="primary">Visit project</Button>
         </Card.Body>
       </Card>
       <Card style={cardStyle}>
-        <Card.Img variant="top" src="holder.js/100px180" />
+        <Card.Img variant="top" src={hanximg} />
         <Card.Body>
-          <Card.Title>Card Title</Card.Title>
+          <Card.Title>HANX!</Card.Title>
           <Card.Text>
-            Some quick example text to build on the card title and make up the
-            bulk of the card's content.
+            Browse some of the many movies of our national American treasure,
+            Tom Hanks. Also be sure to take a look at the Hanks-inspired
+            merchandise that (isn't actually) on sale in the store! My personal
+            favorites are the shower curtain and miniskirt.
           </Card.Text>
-          <Button variant="primary">Visit project</Button>
+          <Button target="_blank" href="https://hanx-app.herokuapp.com/" variant="primary">Visit project</Button>
         </Card.Body>
       </Card>
       <Card style={cardStyle}>
-        <Card.Img variant="top" src="holder.js/100px180" />
+        <Card.Img variant="top" src={pokemonimg} />
         <Card.Body>
-          <Card.Title>Card Title</Card.Title>
+          <Card.Title>Pokemon Battle</Card.Title>
           <Card.Text>
-            Some quick example text to build on the card title and make up the
-            bulk of the card's content.
+            Pick out three of your favorite first-gen Pokemon and have them
+            battle against other users! Submit your name at the top and scroll
+            through the list on the left to create your team.
           </Card.Text>
-          <Button variant="primary">Visit project</Button>
+          <Button target="_blank" href="https://radbahi.github.io/pokemon_battle_frontend/" variant="primary">Visit project</Button>
         </Card.Body>
       </Card>
     </div>
