521d6723cfcaab89452b4afb27dc862c997a88d8
Radouane Bahi <radbahi@gmail.com>
2021-04-11 12:04:22 -0400

    start of redesigning navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,18 +1,17 @@
 import React from 'react'
-import { Media } from 'react-bootstrap'
-
-const smilingimg = require('../local_images/f26efe7e0435ae9972fc079789ab8d59.jpeg')
 
 const NavBar = () => {
   return (
     <div className='whole-navbar'>
-      <Media>
-        <img width={128} height={128} src={smilingimg} alt='Navbar' />
-      </Media>
-      <h5>Radouane Bahi</h5>
-      <a href='/'>Main Page</a>
-      <a href='/projects'>Projects</a>
-      <a href='/links'>Links</a>
+      <a className='links-left' href='/'>
+        Home
+      </a>
+      <a className='links-left' href='/projects'>
+        Projects
+      </a>
+      <a className='links-left' href='/links'>
+        Links
+      </a>
     </div>
   )
 }
