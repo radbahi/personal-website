385148beb8fb3d32e1a39eead50cefa3f90fc0c5
Radouane Bahi <radbahi@gmail.com>
2021-04-11 16:52:00 -0400

    all of main done

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,34 +1,26 @@
 import React from 'react'
-import { Media } from 'react-bootstrap'
-
-const mediaStyle = {
-  // justifyContent: 'center',
-  // padding: 10,
-  backgroundColor: 'white',
-  width: '50vw',
-  // marginLeft: '15rem',
-  // marginRight: '-133rem',
-  marginTop: '9vh',
-  position: 'fixed',
-}
 
 const Main = () => {
   return (
     <div className='main-div'>
-      <Media style={mediaStyle}>
-        <Media.Body>
-          <h5>Welcome to my site!</h5>
-          <p>
-            My name is Radouane Bahi. I am a graduate from the Flatiron School.
-            I got into software engineering from a friend who recommended it to
-            me after I decided to not pursue a career in law enforcement
-            following my graduation from John Jay College. Figures that I'd have
-            a change of heart right after graduation! Please take the time to
-            click on the links to the left leading to my projects and social
-            media/blogs.
-          </p>
-        </Media.Body>
-      </Media>
+      <div className='main-text'>
+        <h1>{'<Hello />,'}</h1>
+        <h1>I'm Radouane Bahi</h1>
+        <p style={{ color: 'white', textShadow: '2px 2px black' }}>
+          Full Stack Web Developer
+        </p>
+        <p style={{ color: 'white', textShadow: '2px 2px black' }}>
+          Based in New York City
+        </p>
+        {/* <p>
+          My name is Radouane Bahi. I am a graduate from the Flatiron School. I
+          got into software engineering from a friend who recommended it to me
+          after I decided to not pursue a career in law enforcement following my
+          graduation from John Jay College. Figures that I'd have a change of
+          heart right after graduation! Please take the time to click on the
+          links to the left leading to my projects and social media/blogs.
+        </p> */}
+      </div>
     </div>
   )
 }
