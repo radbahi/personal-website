5e981899fc61f5ea640224cd901f532b6e70fd94
Radouane Bahi <radbahi@gmail.com>
2021-05-04 12:26:21 -0400

    skillset done. might have to change divs around

--- src/components/Skillset.js
+++ src/components/Skillset.js
@@ -1,7 +1,7 @@
 import React from 'react'
-import javascriptLogo from '../local_images/javascript-logo.svg.png'
+import javascriptLogo from '../local_images/javascript-logo.png'
 import reactLogo from '../local_images/react-logo.png'
-import reduxLogo from '../local_images/redux-logo.jpg'
+import reduxLogo from '../local_images/redux-logo.png'
 import rubyLogo from '../local_images/ruby-logo.png'
 import railsLogo from '../local_images/rails-logo.png'
 
@@ -26,24 +26,25 @@ const Skillset = () => {
             alt='JavaScript'
           />
           <p style={{ color: '#efe4d7', paddingTop: '1vh' }}>JavaScript</p>
-          <div className='skillset-icon-box'>
-            <img className='skillset-icon' src={reactLogo} alt='React' />
-            <p style={{ color: '#efe4d7', paddingTop: '1vh' }}>React</p>
-          </div>
-          <div className='skillset-icon-box'>
-            <img className='skillset-icon' src={reduxLogo} alt='Redux' />
-            <p style={{ color: '#efe4d7', paddingTop: '1vh' }}>Redux</p>
-          </div>
-          <div className='skillset-icons-div-2'>
-            <div className='skillset-icon-box'>
-              <img className='skillset-icon' src={rubyLogo} alt='Ruby' />
-              <p style={{ color: '#efe4d7', paddingTop: '1vh' }}>Ruby</p>
-            </div>
-            <div className='skillset-icon-box'>
-              <img className='skillset-icon' src={railsLogo} alt='Rails' />
-              <p style={{ color: '#efe4d7', paddingTop: '1vh' }}>Rails</p>
-            </div>
-          </div>
+        </div>
+        <div className='skillset-icon-box'>
+          <img className='skillset-icon' src={reactLogo} alt='React' />
+          <p style={{ color: '#efe4d7', paddingTop: '1vh' }}>React</p>
+        </div>
+        <div className='skillset-icon-box'>
+          <img className='skillset-icon' src={reduxLogo} alt='Redux' />
+          <p style={{ color: '#efe4d7', paddingTop: '1vh' }}>Redux</p>
+        </div>
+        <br />
+      </div>
+      <div className='skillset-icons-div-2'>
+        <div className='skillset-icon-box'>
+          <img className='skillset-icon' src={rubyLogo} alt='Ruby' />
+          <p style={{ color: '#efe4d7', paddingTop: '1vh' }}>Ruby</p>
+        </div>
+        <div className='skillset-icon-box'>
+          <img className='skillset-icon' src={railsLogo} alt='Rails' />
+          <p style={{ color: '#efe4d7', paddingTop: '1vh' }}>Rails</p>
         </div>
       </div>
     </div>
