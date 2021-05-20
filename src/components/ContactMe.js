c40c5c8c89ce6ec943b9e9f3706379456ace939b
Radouane Bahi <radbahi@gmail.com>
2021-05-20 12:47:07 -0400

    working on spacing for last section

--- src/components/ContactMe.js
+++ src/components/ContactMe.js
@@ -4,26 +4,31 @@ const ContactMe = () => {
       <h1
         style={{
           textAlign: 'center',
+          paddingBottom: '5%',
+          paddingTop: '5%',
         }}
       >
         I am currently looking for opportunities!
       </h1>
       <br />
-      <div className='contactme-text column'>
-        <p>
-          You can contact me via <strong>LinkedIn</strong> and{' '}
-          <strong>Github</strong>.
-        </p>
-      </div>
-      <div className='contactme-resume column'>
-        <iframe
-          className='pdf'
-          src='pdf/web/viewer.html?file=../../my_resume.pdf'
-          title='My resume'
-          style={{ border: '0' }}
-          width='700vw'
-          height='800vh'
-        />
+      <div className='contactme-content'>
+        <div className='contactme-text column'>
+          <p>
+            You can contact me via <strong>LinkedIn</strong> and{' '}
+            <strong>Github</strong>.
+          </p>
+        </div>
+        <div class='vl column' />
+        <div className='contactme-resume column'>
+          <iframe
+            className='pdf'
+            src='pdf/web/viewer.html?file=../../my_resume.pdf'
+            title='My resume'
+            style={{ border: '0' }}
+            width='670'
+            height='600'
+          />
+        </div>
       </div>
     </section>
   )
