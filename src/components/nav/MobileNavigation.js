16c99de2c7e1764e732e8bcd5b234f68dc7f4513
Radouane Bahi <radbahi@gmail.com>
2021-05-28 18:59:25 -0400

    hamburger!

--- src/components/nav/MobileNavigation.js
+++ src/components/nav/MobileNavigation.js
@@ -1,9 +1,18 @@
 import NavLinks from './NavLinks'
+import { CgMenuBoxed } from 'react-icons/cg'
+import { useState } from 'react'
 
 const MobileNavigation = () => {
+  const [open, setOpen] = useState(false)
   return (
     <nav className='mobile-navigation'>
-      <NavLinks />
+      <CgMenuBoxed
+        className='hamburger'
+        size='40px'
+        color='white'
+        onClick={() => setOpen(!open)}
+      />
+      {open && <NavLinks />}
     </nav>
   )
 }
