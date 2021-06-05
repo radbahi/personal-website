ee196ca91f81c271e654ca9926f87f493b8395a9
Radouane Bahi <radbahi@gmail.com>
2021-06-05 12:49:23 -0400

    final push???

--- src/components/nav/NavLinks.js
+++ src/components/nav/NavLinks.js
@@ -2,19 +2,19 @@ const NavLinks = ({ isMobile, closeMobileMenu }) => {
   return (
     <ul className='navlinks'>
       <li onClick={() => isMobile && closeMobileMenu()}>
-        <a href='#contactme-section'>Contact Me</a>
-      </li>
-      <li onClick={() => isMobile && closeMobileMenu()}>
-        <a href='#project-section'>Projects</a>
-      </li>
-      <li onClick={() => isMobile && closeMobileMenu()}>
-        <a href='#skillset-section'>Skills</a>
+        <a href='#main-section'>Main</a>
       </li>
       <li onClick={() => isMobile && closeMobileMenu()}>
         <a href='#aboutme-section'>About Me</a>
       </li>
       <li onClick={() => isMobile && closeMobileMenu()}>
-        <a href='#main-section'>Main</a>
+        <a href='#skillset-section'>Skills</a>
+      </li>
+      <li onClick={() => isMobile && closeMobileMenu()}>
+        <a href='#project-section'>Projects</a>
+      </li>
+      <li onClick={() => isMobile && closeMobileMenu()}>
+        <a href='#contactme-section'>Contact Me</a>
       </li>
     </ul>
   )
